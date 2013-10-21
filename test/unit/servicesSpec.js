09e93fd5b37a28476822d30ee849b73a76c899fa
Matthias Lienau <matthias@mlienau.de>
2013-10-21 23:26:21 +0200

    Simple check on current version.

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -8,7 +8,7 @@ describe('service', function() {
 
   describe('version', function() {
     it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.0.1');
+      expect(version).toEqual('0.1.0');
     }));
   });
 
