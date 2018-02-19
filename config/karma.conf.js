612615e983358449c1ea75e985af70876d1a1d47
atufkas <matthias@mlienau.de>
2018-02-19 13:41:17 +0100

    Added script which runs karma unit and e2 tests + corrected single-run mode for unit test when in Travis CI env.

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
     },
 
     customLaunchers: {
-    Chrome_travis_ci: {
+      Chrome_travis_ci: {
         base: 'Chrome',
         flags: ['--no-sandbox']
       }
@@ -38,5 +38,6 @@ module.exports = function(config) {
 
   if (process.env.TRAVIS) {
     config.browsers = ['Chrome_travis_ci'];
+    config.singleRun = true;
   }
 }
---
+++ scripts/all-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+BASE_DIR=`dirname $0`
+$BASE_DIR/test.sh
+$BASE_DIR/e2e-test.sh
